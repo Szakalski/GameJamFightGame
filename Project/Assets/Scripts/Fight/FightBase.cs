e79129b7a47837435c3ec8316c2fd26db69da824
Szakalski <szakaalski@gmail.com>
2014-10-18 18:20:38 +0200

    Add: EventManager (opensource)

---
+++ Project/Assets/Scripts/Fight/FightBase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightBase : MonoBehaviour {
+
+	private SceneManager _sceneManager;
+
+	private void Awake()
+	{
+		_sceneManager = SceneManager.getInstance();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
