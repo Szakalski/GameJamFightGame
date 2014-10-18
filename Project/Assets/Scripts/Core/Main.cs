53a0e38f1d42fb683fd6fd4c1239f9eb3673cbf5
Szakalski <szakaalski@gmail.com>
2014-10-18 16:04:37 +0200

    Added: Boostrap + Scene Manager

---
+++ Project/Assets/Scripts/Core/Main.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	
+	private static Main _instance;	
+	private SceneManager _sceneManager;
+	
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		
+		_sceneManager = SceneManager.getInstance();
+	}
+	
+	void Start()
+	{
+		_sceneManager.loadLevel("MainMenu");
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;	
+		Screen.orientation = ScreenOrientation.Landscape;
+	}
+	
+	public static Main getInstance()
+	{
+		if ( _instance == null )
+		{
+			#if UNITY_EDITOR
+				Debug.Log("Finding _main");
+			#endif
+			GameObject lGO = GameObject.Find("_main");
+			_instance = lGO.GetComponent<Main>();
+		}
+		
+		return _instance;
+	}
+}
---
+++ Project/Assets/Scripts/Core/SceneManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+	
+	private static SceneManager _instance;
+	private GameObject _loadingPanel;
+	
+	public static SceneManager getInstance()
+	{
+		if ( _instance == null )
+		{
+			#if UNITY_EDITOR
+				Debug.Log("Had to create new SceneManager!");
+			#endif
+			GameObject go = GameObject.Find ("_main");
+			if(go == null)
+				go = new GameObject("SceneManager");
+			_instance = go.AddComponent<SceneManager>();
+		}
+		
+		return _instance;
+	}
+	
+	public void loadLevel(string pName, string uiRoot = "UI Root")
+	{
+		#if UNITY_EDITOR
+			Debug.Log ("SceneManager -> loadLevel: " + pName);
+		#endif
+		
+		Application.LoadLevel(pName);
+	}
+}
