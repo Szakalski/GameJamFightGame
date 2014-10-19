e92e8a8dbcc2b5ec8d33e788433a85cb75e33358
Szakalski <szakaalski@gmail.com>
2014-10-19 19:06:32 +0200

    Add; Player dodge (phone only)

---
+++ Project/Assets/Scripts/Fight/DodgeState.cs
@@ -0,0 +1,6 @@
+public enum DodgeState
+{
+	NO_DODGE = 0,
+	DODGE_LEFT = 1,
+	DODGE_RIGHT = 2
+}
--- Project/Assets/Scripts/Fight/PlayerController.cs
+++ Project/Assets/Scripts/Fight/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 	private FightBase _fightBase;
 	private float playerStamina = 100f;
 
+	private DodgeState _playerDodge;
+
 	private Animator anim;
 	int attackMidRight = Animator.StringToHash("AttackMidR");
 	int attackMidLeft = Animator.StringToHash("AttackMidL");
@@ -13,6 +15,18 @@ public class PlayerController : MonoBehaviour {
 //	int attackTopLeft = Animator.StringToHash("AttackMidL");
 //	int attackLowLeft = Animator.StringToHash("AttackMidL");
 //	int attackLowLeft = Animator.StringToHash("AttackMidL");
+
+	int dodgeLeft = Animator.StringToHash("DodgeLeft");
+	int dodgeRight = Animator.StringToHash("DodgeRight");
+	int dodgeReturn = Animator.StringToHash("DodgeReturn");
+	
+	public DodgeState PlayerDodge
+	{
+		get
+		{
+			return _playerDodge;
+		}
+	}
 
 	private void Awake()
 	{
@@ -24,8 +38,48 @@ public class PlayerController : MonoBehaviour {
 		anim = GetComponent<Animator>();
 	}
 
+	private void FixedUpdate()
+	{
+		CheckGyroscopeDodge(Input.acceleration);
+	}
+
+	private void CheckGyroscopeDodge(Vector3 acceleration)
+	{
+		if(acceleration.normalized.x < -0.35f)
+			_playerDodge = DodgeState.DODGE_LEFT;
+		else if(acceleration.normalized.x > 0.35f)
+			_playerDodge = DodgeState.DODGE_RIGHT;
+		else
+			_playerDodge = DodgeState.NO_DODGE;
+
+		SetDodgeAnimation(_playerDodge);
+	}
+
+	private void SetDodgeAnimation(DodgeState playerDodge)
+	{
+		if(_playerDodge == DodgeState.NO_DODGE)
+		{			
+			anim.ResetTrigger(dodgeLeft);
+			anim.ResetTrigger(dodgeRight);
+			anim.SetTrigger(dodgeReturn);
+		}
+		else if(_playerDodge == DodgeState.DODGE_LEFT)
+		{
+			anim.ResetTrigger(dodgeReturn);
+			anim.SetTrigger(dodgeLeft);
+		}
+		else if(_playerDodge == DodgeState.DODGE_RIGHT)
+		{
+			anim.ResetTrigger(dodgeReturn);
+			anim.SetTrigger(dodgeRight);
+		}
+	}
+
 	public void OnTapArea(int target)
 	{
+		if(_playerDodge == DodgeState.DODGE_LEFT || _playerDodge == DodgeState.DODGE_RIGHT)
+			return;
+
 		CustomEvent hitEvent = new CustomEvent(FightBase.E_SEND_HIT);
 		hitEvent.arguments.Add("target", (PunchTarget)target);
 
